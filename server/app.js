4419add7c076e010c6fff114bb9f7d263c69262d
Carlos <clockwerkz@gmail.com>
2021-05-28 16:19:40 -0700

    basic mongodb setup

--- server/app.js
+++ server/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
+require ('dotenv').config();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.user}:${process.env.pw}@cluster0.xcqlp.mongodb.net/games?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log("MongoDB connected....")
+  const collection = client.db("games").collection("campaigns");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 
 const app = express();
 const PORT = 4050;
