d7e805ea9a1063ab90a77525abf4343762e1475a
Carlos <clockwerkz@gmail.com>
2021-05-30 15:43:11 -0700

    applying formatting to files

--- server/app.js
+++ server/app.js
@@ -1,31 +1,33 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
-require ('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xcqlp.mongodb.net/campaign-tracker?retryWrites=true&w=majority`;
 
-mongoose.connect(uri, {
+mongoose
+  .connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then((result) => {
-        console.log("Connected to db via mongoose");
-    })
-    .catch(err => console.log(err));
-
-
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Connected to db via mongoose');
+  })
+  .catch((err) => console.log(err));
 
 const app = express();
 const PORT = 4050;
 
 const schema = require('./schema/schema');
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
-    graphiql : true
-}));
+    graphiql: true,
+  }),
+);
 
-app.listen(PORT, ()=>{
-    console.log(`App listening on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,117 +1,116 @@
 const graphql = require('graphql');
 
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLSchema,
-    GraphQLID,
-    GraphQLInt,
-    GraphQLList
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
 } = graphql;
 
-
 const campaigns = [
-    {
-        id: '1',
-        name: "Starter Campaign",
-        currentAdventure: "Level 1"
-    },
-    {
-        id: '2',
-        name: "Starter Campaign - Part 2",
-        currentAdventure: "Level 2"
-    }
-]
+  {
+    id: '1',
+    name: 'Starter Campaign',
+    currentAdventure: 'Level 1',
+  },
+  {
+    id: '2',
+    name: 'Starter Campaign - Part 2',
+    currentAdventure: 'Level 2',
+  },
+];
 
 const characters = [
-    {
-        id: '1',
-        name: "Barbar",
-        class: "Barbarian",
-        gold: 34
-    },
-    {
-        id: '2',
-        name: "Germ",
-        class: "Wizard",
-        gold: 34,
-        creator: '2'
-    }
-]
+  {
+    id: '1',
+    name: 'Barbar',
+    class: 'Barbarian',
+    gold: 34,
+  },
+  {
+    id: '2',
+    name: 'Germ',
+    class: 'Wizard',
+    gold: 34,
+    creator: '2',
+  },
+];
 
 const items = [
-    {
-        id: '1',
-        name: 'Short sword',
-        sellPrice: 10,
-    },
-    {
-        id: '2',
-        name: 'Health Potion',
-        sellPrice: 5,
-        campaign: '1'
-    }
-]
+  {
+    id: '1',
+    name: 'Short sword',
+    sellPrice: 10,
+  },
+  {
+    id: '2',
+    name: 'Health Potion',
+    sellPrice: 5,
+    campaign: '1',
+  },
+];
 
 const ItemType = new GraphQLObjectType({
-    name: 'item',
-    fields: ()=>({
-        id: { type: GraphQLID },
-        name: { type : GraphQLString },
-        sellPrice: { type: GraphQLInt },
-        campaign: { type: GraphQLID }
-    })
-})
+  name: 'item',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    sellPrice: { type: GraphQLInt },
+    campaign: { type: GraphQLID },
+  }),
+});
 
 const CampaignType = new GraphQLObjectType({
-    name: 'campaign',
-    fields: ()=>({
-        id: { type: GraphQLID },
-        name: { type : GraphQLString },
-        currentAdventure: { type: GraphQLString },
-        characters: {
-            type: new GraphQLList(CharacterType),
-            resolve(campaign, args) {
-                return characters.filter(char => !char.creator || char.creator === campaign.id)
-            }
-        }
-    })
+  name: 'campaign',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    currentAdventure: { type: GraphQLString },
+    characters: {
+      type: new GraphQLList(CharacterType),
+      resolve(campaign, args) {
+        return characters.filter(
+          (char) => !char.creator || char.creator === campaign.id,
+        );
+      },
+    },
+  }),
 });
 
 const CharacterType = new GraphQLObjectType({
-    name: 'character',
-    fields: ()=>({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        class: { type: GraphQLString},
-        gold: { type: GraphQLInt},
-        creator : { type: GraphQLID },
-    })
-})
-
-const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        campaign: {
-            type: CampaignType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                // code to get data from db / other source
-                return campaigns.find(campaign => campaign.id === args.id)
-            }
-        },
-        character: {
-            type: CharacterType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                return characters.find(char => char.id === args)
-            }
-        }
-    }
+  name: 'character',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    class: { type: GraphQLString },
+    gold: { type: GraphQLInt },
+    creator: { type: GraphQLID },
+  }),
 });
 
-
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    campaign: {
+      type: CampaignType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return campaigns.find((campaign) => campaign.id === args.id);
+      },
+    },
+    character: {
+      type: CharacterType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return characters.find((char) => char.id === args);
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery,
 });
